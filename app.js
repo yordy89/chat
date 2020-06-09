0c0569c869cb081fdfcc8f1e43b654bb1f307828
Yordanys Albelo Talavera <yordy@vra.ecasa.avianet.cu>
2020-06-09 04:01:01 -0500

    primer commit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import path from 'path'
+const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const config = require('./config')
 
-const app = express()
 
 //BD
 import mongoose from 'mongoose'
@@ -23,6 +26,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 //Rutas
+
+app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
@@ -31,9 +37,12 @@ app.get('/',(req,res)=>{
 const history = require('connect-history-api-fallback')
 app.use(history())
 
-
+io.on('connection', ()=>{
+    console.log("Conectado")
+})
 
 //inicializando el servidor
 app.listen(config.port,()=>{
     console.log("Server on port 4000")
 })
+
